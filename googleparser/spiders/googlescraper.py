c73379d05e7655342172a95f4ee13f18b00e78aa
tahabi <abizerkl@gmail.com>
2012-08-07 18:34:43 -0700

    updating script file

--- googleparser/spiders/googlescraper.py
+++ googleparser/spiders/googlescraper.py
@@ -1,40 +1,26 @@
-from scrapy.spider import BaseSpider
-from scrapy.http import Request
+#! /usr/bin/env python 
+
 from bs4 import BeautifulSoup
-import time, urllib
+import urllib, requests, sys
 
-class GoogleParser(BaseSpider):
+def google(query, urloptions=None):
+    url = "https://www.google.com/search?q=" + urllib.quote(query)
+    if urloptions is not None:
+        for opt in urloptions.keys():
+            url += "&%s=%s" % (opt, urloptions[opt])
+    soup = BeautifulSoup(requests.get(url).content)
+    output = []
+    for link in soup('a'):
+        if "/url?q=" in link['href']:
+            output.append(str(link['href'].split('&')[0][7:]))
 
-    name = "googleparser"
+    return output
+        
+if len(sys.argv) < 2:
+    sys.exit("Usage is (python) google 'query'\n")
+else:
+    print  "\n".join(google(sys.argv[1], {"start":10}))
+
     
-    allowed_urls = ["google.com"]
-
-    max_results = None
-    count = 10
-    query = ""
-    _url = "http://www.google.com/search?q="
-    
-    def start_requests(self):
-
-        if not self.query:
-            self.query = str(urllib.quote(raw_input("What would you like to search Google for?\n")))
-            self.max_results = input("How many results would you like before the spider stops?\n")
-            self._url += self.query + "&start="
         
-        return [Request(url=self._url + str(self.count), callback=self.parse)]
 
-    def parse(self, response):
-        if self.count >= self.max_results:
-            exit
-        else:
-            self.count += 10
-
-        soup = BeautifulSoup(response.body)
-        
-        for link in soup('a'):
-            if "/url?q=" in link['href']:
-                print str(link['href'].split('&')[0][7:]) + "\n"
-                print " -- " + str(link.find_next('div', 's').find('span', 'st')) + "\n"
-            
-        
-        yield Request(url=self._url + str(self.count), callback=self.parse)
