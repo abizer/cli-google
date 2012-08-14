f26a8ffa289126b39dcde559cbd20a9a6e161fc0
tahabi <Tahabi@Tahabi.(none)>
2012-08-13 19:38:25 -0700

    added name option

--- google.py
+++ google.py
@@ -3,35 +3,73 @@
 from bs4 import BeautifulSoup
 import urllib, requests, sys
 
-def google(query, max_results, urloptions = None):
+def google(query, opts = None, max_results = None, urloptions = None):
     url = "https://www.google.com/search?q=" + urllib.quote(query)
     if urloptions is not None:
         for opt in urloptions.keys():
             url += "&%s=%s" % (opt, urloptions[opt])
-
-    output = []
-    count = 10
     
-    while count <= max_results:
-        count += 10
-        url += "&start=" + str(count)
-        soup = BeautifulSoup(requests.get(url).content)
-        for link in soup('a'):
-            if "/url?q=" in link['href']:
+    output = []
+    count = 0
+    
+   # while count <= max_results:
+    url += "&start=" + str(count)
+    count += 10
+    soup = BeautifulSoup(requests.get(url).content)
+    for link in soup('a'):
+        if "/url?q=" in link['href']:
+            if opts is not None:
+                if opts == "links":
+                    output.append(str(link['href'].split('&')[0][7:]))
+                elif opts == "names":
+                    try:
+                        output.append(str(link.text))
+                    except:
+                        output.append("Name Contains Illegal Unicode Characters!")
+                elif opts == "descs":
+                    try:
+                        output.append(str(link.text))
+                    except:
+                        output.append("Description Contains Illegal Unicode")  
+                else:
+                    sys.exit("Unrecognized value '%s' for paramater opts! Recognized Parameters: names | links | descs" % opts)
+            else:
                 output.append(str(link['href'].split('&')[0][7:]))
 
-    return output
-        
+    return (output, url)
+
+
+
+    
 if len(sys.argv) < 2:
-    sys.exit("Usage is (python) google 'query' [max_results [urloptions]]\n")
+    sys.exit("Usage is (python) google 'query' [options (names || descs || links)[max_results [urloptions]]]\n")
 else:
     try:
-        max_results = int(sys.argv[2])
+        opts = sys.argv[2]
+    except:
+        opts = None
+    try:
+        max_results = int(sys.argv[3])
     except:
         max_results = 10
     try:
-        urloptions = sys.argv[3]
+        urloptions = sys.argv[4]
     except:
         urloptions = None
-    print  "\n".join(google(sys.argv[1], max_results, urloptions))
+        
+    (results, url) = google(sys.argv[1], opts, max_results, urloptions)
 
+    print "\n".join(results)
+    
+    
+  #  print "\n".join(results[0])
+  #  print "\n" + results[1]
+
+    
+
+    
+    
+    """"for x in xrange(0, max_results):
+        print results[0][x] + "\t" + results[1][x] + "\n"
+
+"""
